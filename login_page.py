7cbdcfceb50bb7db8b063ce7fa2425b48f6a23ad
DILEEPKUMAR <110006271+JOHANDILEEP21@users.noreply.github.com>
2023-11-23 19:10:08 +0530

    Update login_page.py

--- login_page.py
+++ login_page.py
@@ -51,52 +51,52 @@ import models
 def login_page(credentials, Authenticator, username, usernames):
     st.title(':red[UniJack]')
     st.header('Resume Screening App')
-    try:
-        info, info1 = st.columns(2)
-        # let user see app
-        st.sidebar.subheader(f'Welcome {username.capitalize()}')
-        col1, col2, col3, col4, col5 = st.columns(5)
-        # Now you can use the columns to add content
-        with col5:
-            Authenticator.logout('Log out') #, 'sidebar')
-        # md = ml_model()
-        #st.write(md)
-        start = st.checkbox('Click here to start')
-        if start:
-            df = pd.read_csv('https://github.com/JOHANDILEEP21/Resume_parsing/blob/main/UpdatedResumeDataSet.csv')
-            st.dataframe(df)
+    # try:
+    info, info1 = st.columns(2)
+    # let user see app
+    st.sidebar.subheader(f'Welcome {username.capitalize()}')
+    col1, col2, col3, col4, col5 = st.columns(5)
+    # Now you can use the columns to add content
+    with col5:
+        Authenticator.logout('Log out') #, 'sidebar')
+    # md = ml_model()
+    #st.write(md)
+    start = st.checkbox('Click here to start')
+    if start:
+        df = pd.read_csv('https://github.com/JOHANDILEEP21/Resume_parsing/blob/main/UpdatedResumeDataSet.csv')
+        st.dataframe(df)
+    
+        df['Resume'] = df['Resume'].apply(lambda x: models.clean_resume(x))
+    
+        le = LabelEncoder()
+        le.fit(df['Category'])
+        df['Category'] = le.transform(df['Category'])
+        st.dataframe(df)
+    
+        tfidf = TfidfVectorizer(stop_words='english')
+        tfidf = tfidf.fit(df['Resume'])
+        requredTaxt  = tfidf.transform(df['Resume'])
+        #st.write(requredTaxt)
+        X_train, X_test, y_train, y_test = train_test_split(requredTaxt, df['Category'], test_size=0.2, random_state=42)
+    
+        clf = OneVsRestClassifier(KNeighborsClassifier())
+        clf = clf.fit(X_train,y_train)
+        ypred = clf.predict(X_test)
+        #st.write(accuracy_score(y_test,ypred))
+    
+        pickle.dump(tfidf,open('tfidf.pkl','wb'))
+        pickle.dump(clf, open('clf.pkl', 'wb'))
         
-            df['Resume'] = df['Resume'].apply(lambda x: models.clean_resume(x))
-        
-            le = LabelEncoder()
-            le.fit(df['Category'])
-            df['Category'] = le.transform(df['Category'])
-            st.dataframe(df)
-        
-            tfidf = TfidfVectorizer(stop_words='english')
-            tfidf = tfidf.fit(df['Resume'])
-            requredTaxt  = tfidf.transform(df['Resume'])
-            #st.write(requredTaxt)
-            X_train, X_test, y_train, y_test = train_test_split(requredTaxt, df['Category'], test_size=0.2, random_state=42)
-        
-            clf = OneVsRestClassifier(KNeighborsClassifier())
-            clf = clf.fit(X_train,y_train)
-            ypred = clf.predict(X_test)
-            #st.write(accuracy_score(y_test,ypred))
-        
-            pickle.dump(tfidf,open('tfidf.pkl','wb'))
-            pickle.dump(clf, open('clf.pkl', 'wb'))
-            
-            # Load the trained classifier
-            # clf = pickle.load(open('clf.pkl', 'rb'))
-            st.write([tfidf, clf])
-        
-            # return clf, tfidf
-            res = resume.resume_parsing(clf, tfidf)
-            res
-        else:
-            st.write('Please upload dataset for model training purposes')
+        # Load the trained classifier
+        # clf = pickle.load(open('clf.pkl', 'rb'))
+        st.write([tfidf, clf])
+    
+        # return clf, tfidf
+        res = resume.resume_parsing(clf, tfidf)
+        res
+    else:
+        st.write('Please upload dataset for model training purposes')
                     
-    except Exception as e:
-        st.warning(e)
+# except Exception as e:
+#     st.warning(e)
     
