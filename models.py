3d3fb3ea5585584da0c2dd212622894c7e67ab9e
DILEEPKUMAR <110006271+JOHANDILEEP21@users.noreply.github.com>
2023-11-23 17:57:56 +0530

    Update models.py

--- models.py
+++ models.py
@@ -33,66 +33,70 @@ def extract_data(feed):
 
 # web app
 def main(md):
-    #st.title("Resume Screening App")
-    uploaded_file = st.file_uploader('Upload Resume', type='pdf')
+    try:
+        #st.title("Resume Screening App")
+        uploaded_file = st.file_uploader('Upload Resume', type='pdf')
+        
+        if uploaded_file is not None:
+            try:
+                df = extract_data(uploaded_file)
+                resume_text = str(df)
+                #st.text(df)
+                #resume_bytes = uploaded_file.read()
+                #resume_text = remove_null_characters(uploaded_file.read().decode('utf-8'))
+            except UnicodeDecodeError as uce:
+                # If UTF-8 decoding fails, try decoding with 'latin-1'
+                #resume_text = remove_null_characters(uploaded_file.read().decode('latin-1'))
+                st.write(uce)
     
-    if uploaded_file is not None:
-        try:
-            df = extract_data(uploaded_file)
-            df = str(df)
-            #st.text(df)
-            #resume_bytes = uploaded_file.read()
-            #resume_text = remove_null_characters(uploaded_file.read().decode('utf-8'))
-        except UnicodeDecodeError as uce:
-            # If UTF-8 decoding fails, try decoding with 'latin-1'
-            #resume_text = remove_null_characters(uploaded_file.read().decode('latin-1'))
-            st.write(uce)
+            clf, tfidf = md
+            #tfidf = pickle.load(open('https://github.com/JOHANDILEEP21/Resume_parsing/blob/main/tfidf.pkl', 'rb'))
+            #tfidf = pickle.load(open('https://github.com/JOHANDILEEP21/Resume_parsing/blob/main/tfidf.pkl'))
+            
+            cleaned_resume = clean_resume(resume_text)
+            cleaned_resume = str(cleaned_resume)
+            #st.text(type(cleaned_resume))
+            input_features = tfidf.transform([cleaned_resume])
+            prediction_id = clf.predict(input_features)[0]
+            #st.write(prediction_id)
+    
+            # Map category ID to category name
+            category_mapping = {
+                15: "Java Developer",
+                23: "Testing",
+                8: "DevOps Engineer",
+                20: "Python Developer",
+                24: "Web Designing",
+                12: "HR",
+                13: "Hadoop",
+                3: "Blockchain",
+                10: "ETL Developer",
+                18: "Operations Manager",
+                6: "Data Science",
+                22: "Sales",
+                16: "Mechanical Engineer",
+                1: "Arts",
+                7: "Database",
+                11: "Electrical Engineering",
+                14: "Health and fitness",
+                19: "PMO",
+                4: "Business Analyst",
+                9: "DotNet Developer",
+                2: "Automation Testing",
+                17: "Network Security Engineer",
+                21: "SAP Developer",
+                5: "Civil Engineer",
+                0: "Advocate",
+            }
+    
+            category_name = category_mapping.get(prediction_id, "Unknown")
+    
+            st.write("Predicted Category:", category_name)
+            
+            return cleaned_resume, True
+        else:
+            st.text('Error in this function')
 
-        clf, tfidf = md
-        #tfidf = pickle.load(open('https://github.com/JOHANDILEEP21/Resume_parsing/blob/main/tfidf.pkl', 'rb'))
-        #tfidf = pickle.load(open('https://github.com/JOHANDILEEP21/Resume_parsing/blob/main/tfidf.pkl'))
-        
-        cleaned_resume = clean_resume(df)
-        cleaned_resume = str(cleaned_resume)
-        #st.text(type(cleaned_resume))
-        input_features = tfidf.transform([cleaned_resume])
-        prediction_id = clf.predict(input_features)[0]
-        #st.write(prediction_id)
-
-        # Map category ID to category name
-        category_mapping = {
-            15: "Java Developer",
-            23: "Testing",
-            8: "DevOps Engineer",
-            20: "Python Developer",
-            24: "Web Designing",
-            12: "HR",
-            13: "Hadoop",
-            3: "Blockchain",
-            10: "ETL Developer",
-            18: "Operations Manager",
-            6: "Data Science",
-            22: "Sales",
-            16: "Mechanical Engineer",
-            1: "Arts",
-            7: "Database",
-            11: "Electrical Engineering",
-            14: "Health and fitness",
-            19: "PMO",
-            4: "Business Analyst",
-            9: "DotNet Developer",
-            2: "Automation Testing",
-            17: "Network Security Engineer",
-            21: "SAP Developer",
-            5: "Civil Engineer",
-            0: "Advocate",
-        }
-
-        category_name = category_mapping.get(prediction_id, "Unknown")
-
-        st.write("Predicted Category:", category_name)
-        
-        return cleaned_resume, True
-    else:
-        st.text('Error in this function')
+    except Exception as e:
+        st.text(e)
 
