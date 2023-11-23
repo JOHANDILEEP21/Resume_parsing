2316f61baeca9574b73c8a5111a2baa52f3cab8a
DILEEPKUMAR <110006271+JOHANDILEEP21@users.noreply.github.com>
2023-11-23 18:21:16 +0530

    Update login_page.py

--- login_page.py
+++ login_page.py
@@ -31,8 +31,8 @@ def model():
         X_train, X_test, y_train, y_test = train_test_split(requredTaxt, df['Category'], test_size=0.2, random_state=42)
 
         clf = OneVsRestClassifier(KNeighborsClassifier())
-        clf.fit(X_train,y_train)
-        #ypred = clf.predict(X_test)
+        clf = clf.fit(X_train,y_train)
+        ypred = clf.predict(X_test)
         #st.write(accuracy_score(y_test,ypred))
 
         pickle.dump(tfidf,open('tfidf.pkl','wb'))
