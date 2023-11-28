2e85dbe651b5029e30bfa9aed52267344e04af1b
DILEEPKUMAR <110006271+JOHANDILEEP21@users.noreply.github.com>
2023-11-29 00:10:47 +0530

    Update resume.py

--- resume.py
+++ resume.py
@@ -24,21 +24,18 @@ def resume_parsing():
         # st.text(resume_text)
         if mdd:
             input_resume = resume_text
-            # st.text(input_resume)
             input_resume = ''.join(input_resume)
-            # st.text(input_resume)
-
+            
             contact_info, resume_skills = resume_screen.parse_resume(input_resume)
-            st.text([contact_info, resume_skills])
+            
             required_skills = st.text_input('Enter the skills that you need')
             #input_skills = "Data Science, Data Analysis, Database, SQL, Machine Learning, tableau, data mining, pandas, numpy, nlp, python, mongodb"
 
             req_skills = required_skills.lower().split(", ")
             #st.text(['Required Skills: ', req_skills])
             resume_skills = resume_screen.get_skills(input_resume.lower())  #unique_skills(get_skills(input_resume.lower()))
-            # st.text(resume_skills)
-            # resume_skills = set(resume_skills)
-            st.text(resume_skills)
+            resume_skills = set(resume_skills)
+            #st.text(resume_skills)
             score = 0
             for x in req_skills:
                 if x in resume_skills:
